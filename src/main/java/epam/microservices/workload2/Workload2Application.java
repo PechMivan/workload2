133780c1f0b45ce0dbb3920e7c242083c69816ab
PechMivan <pech.mivan@gmail.com>
2024-03-22 17:44:41 -0600

    first commit

---
+++ src/main/java/epam/microservices/workload2/Workload2Application.java
@@ -0,0 +1,13 @@
+package epam.microservices.workload2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workload2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workload2Application.class, args);
+	}
+
+}
